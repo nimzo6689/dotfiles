28f11e8ace273266eb0eba2e8241e90d4d28a506
nimzo6689 <nimzo.i.d.6689@gmail.com>
2026-04-29 22:21:55 +0900

    feat: Update fish config to initialize SSH agent and AWS completions

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -2,6 +2,15 @@ if status is-interactive
 # Commands to run in interactive sessions can go here
 end
 
+if not set -q SSH_AUTH_SOCK
+    eval (ssh-agent -c) > /dev/null
+    ssh-add ~/.ssh/id_ed25519 2>/dev/null
+end
+
+if test -f /usr/local/bin/aws_completer
+    complete --command aws --no-files --arguments '(string split " " (env COMP_LINE=(commandline -pc) COMP_POINT=(string length (commandline -pc)) /usr/local/bin/aws_completer))'
+end
+
 /home/nimzo/.local/bin/mise activate fish | source # added by https://mise.run/fish
 
 eval (oh-my-posh init fish --config ~/.cache/oh-my-posh/themes/emodipt.omp.json)
