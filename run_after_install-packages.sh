0d917bc842afbd8d4dfa5d4aa403737db3aa9504
nimzo6689 <nimzo.i.d.6689@gmail.com>
2026-05-04 22:07:07 +0900

    feat: Remove chezmoi from tools and add script to check for mise runtimes

---
+++ run_after_install-packages.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Checking for new mise runtimes..."
+if command -v mise &> /dev/null; then
+    ~/.local/bin/mise install -y
+else
+    echo "mise is not installed. Skipping..."
+fi
