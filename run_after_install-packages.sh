f21fee6431456485cc1a2fad9542059292b4e78b
nimzo6689 <nimzo.i.d.6689@gmail.com>
2026-05-07 23:15:16 +0900

    feat: Update mise check in run_after_install-packages.sh to use file existence test

--- run_after_install-packages.sh
+++ run_after_install-packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Checking for new mise runtimes..."
-if command -v mise &> /dev/null; then
+if test -f ~/.local/bin/mise &> /dev/null; then
     ~/.local/bin/mise install -y
 else
     echo "mise is not installed. Skipping..."
