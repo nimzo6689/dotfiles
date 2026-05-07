94604fea42529272bae58b43a400abe38a464880
nimzo6689 <nimzo.i.d.6689@gmail.com>
2026-05-07 15:18:46 +0900

    feat: Refactor chezmoi function for Bitwarden session management in fish config

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -11,7 +11,21 @@ if test -f /usr/local/bin/aws_completer
     complete --command aws --no-files --arguments '(string split " " (env COMP_LINE=(commandline -pc) COMP_POINT=(string length (commandline -pc)) /usr/local/bin/aws_completer))'
 end
 
-/home/nimzo/.local/bin/mise activate fish | source # added by https://mise.run/fish
+/home/nimzo/.local/bin/mise activate fish | source
+
+# ------------------------------------
+# Chezmoi with Bitwarden session management
+# ------------------------------------
+function chez
+    if not set -q BW_SESSION; or test -z "$BW_SESSION"
+        echo "Bitwarden session not found. Unlocking..."
+        set -gx BW_SESSION (bw unlock --raw)
+    end
+    
+    if test -n "$BW_SESSION"
+        command chezmoi $argv
+    end
+end
 
 eval (oh-my-posh init fish --config emodipt)
 zoxide init fish | source
